f3548513cfdb9cd60ab53d04d1e495df231aba5f
miami78 <miamilarry78@gmail.com>
2019-08-23 19:36:24 +0400

    update event handling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ class App extends Component {
       .then(Response => Response.json())
       .then(users => this.setState({ monsters: users }));
   }
-  render(){
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
@@ -24,7 +29,7 @@ class App extends Component {
       <div className="App">
           <SearchBox 
             placeholder='search monsters'
-            handleChange={e => this.setState({ searchField: e.target.value })}
+            handleChange={this.handleChange}
           />
           <CardList monsters={filteredMonsters} />
       </div>
