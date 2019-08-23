78085e746c79252dcc5f5f1978b78f8f137fb72c
miami78 <miamilarry78@gmail.com>
2019-08-23 20:51:32 +0400

    Updates

---
+++ precache-manifest.cc3823920e0ca7b437fbe0e938a28807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01607e5fafe20548b4180883a59c39d",
+    "url": "/monsters-robodex/index.html"
+  },
+  {
+    "revision": "9e4ac60289573902cde2",
+    "url": "/monsters-robodex/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-robodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "9e4ac60289573902cde2",
+    "url": "/monsters-robodex/static/js/main.5c2725e4.chunk.js"
+  },
+  {
+    "revision": "11203d06d323288c41f9",
+    "url": "/monsters-robodex/static/js/runtime~main.6f73ebe5.js"
+  }
+]);
